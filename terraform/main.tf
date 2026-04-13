69956b9462ee68f117c6e106b8008e3af68a3558
Nattasak Chonmanat <nattasak.cho@dome.tu.ac.th>
2026-04-13 21:05:50 +0700

    rearrange folder structure

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+# Not implemented yet
+
+# TODO: Implement the following resources in Terraform:
+# - Create DynamoDB table for storing prioritization records
+# - Create Lambda function for handling synchronous, async processing of prioritization requests
+# - Create queue and dead letter queue (e.g., SQS) for decoupling the processing of prioritization requests
+# - Create SNS topic for sending notifications about prioritization results
+# - Create API Gateway endpoint for triggering synchronous request
+# - Create Step Functions state machine for orchestrating the prioritization workflow
